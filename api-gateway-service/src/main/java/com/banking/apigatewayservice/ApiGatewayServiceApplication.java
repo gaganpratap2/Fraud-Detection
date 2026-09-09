69bd0cb3671afe39bb67294ccb9fbe78c90bcc8e
Gagan <gaganpratapsingh61@gmail.com>
2026-09-09 13:26:27 +0530

    initialise project and all microservice

---
+++ api-gateway-service/src/main/java/com/banking/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.banking.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+		System.out.println("hello gagan");
+
+	}
+
+}
